fecc06e57cdd300589250266bc476f6b5dc009f4
Erik Eelde <erik.eelde@gmail.com>
2023-04-30 01:19:25 +0200

    Demos of programmatic and assetBased views

--- app/src/main/java/se/premex/gross/MainActivity.kt
+++ app/src/main/java/se/premex/gross/MainActivity.kt
@@ -1,54 +1,78 @@
 package se.premex.gross
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AddCircle
+import androidx.compose.material.icons.rounded.Create
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import se.premex.gross.ui.OssView
+import androidx.compose.ui.res.stringResource
 import se.premex.gross.ui.theme.GrossTheme
 
+enum class Views {
+    Programmatic, AssetBased
+}
+
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val selectedView = remember { mutableStateOf(Views.Programmatic) }
             GrossTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column {
-                        Greeting("Android")
-                        ProgrammaticOssView()
+                Scaffold(
+                    bottomBar = {
+                        BottomAppBar {
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = {
+                                    selectedView.value = Views.Programmatic
+                                },
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Rounded.Create,
+                                        contentDescription = stringResource(id = R.string.programmatic)
+                                    )
+                                })
+                            NavigationBarItem(
+                                selected = false,
+                                onClick = {
+                                    selectedView.value = Views.AssetBased
+                                },
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Rounded.AddCircle,
+                                        contentDescription = stringResource(id = R.string.assetBased)
+                                    )
+                                })
+                        }
+                    }
+                ) { paddingValues ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        when (selectedView.value) {
+                            Views.Programmatic -> ProgrammaticOssView()
+                            Views.AssetBased -> AssetsOssView()
+                        }
                     }
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GrossTheme {
-        Column {
-            Greeting("Android")
         }
     }
 }
--- app/src/main/java/se/premex/gross/OssArtifactView.kt
+++ app/src/main/java/se/premex/gross/OssArtifactView.kt
@@ -1,5 +1,7 @@
 package se.premex.gross
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -10,6 +12,7 @@ import se.premex.gross.oss.R
 import se.premex.gross.ui.AssetLicenseParser
 import se.premex.gross.ui.ErrorView
 import se.premex.gross.ui.LoadingView
+import se.premex.gross.ui.OssView
 import se.premex.gross.ui.OssViewState
 import se.premex.gross.ui.State
 import java.io.IOException
@@ -28,13 +31,23 @@ fun AssetsOssView() {
                 OssViewState(viewState = State.Success(data = licenseParser.readFromAssets()))
         } catch (ioException: IOException) {
             uiState.value =
-                OssViewState(viewState = State.Failed(errorMessage = ioException.localizedMessage ?: ""))
+                OssViewState(
+                    viewState = State.Failed(
+                        errorMessage = ioException.localizedMessage ?: ""
+                    )
+                )
         }
     }
 
     when (val state = uiState.value.viewState) {
         is State.Failed -> ErrorView(stringResource(id = R.string.error), state.errorMessage)
         is State.Loading -> LoadingView(stringResource(id = R.string.loading))
-        is State.Success -> se.premex.gross.ui.OssView(state.data)
+        is State.Success -> {
+            Column {
+                Text(text = stringResource(id = se.premex.gross.R.string.assetBased))
+
+                OssView(state.data)
+            }
+        }
     }
 }
